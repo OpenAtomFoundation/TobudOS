a52bb94ed241c19eee517bfc0843e2143262fd6f
acevest <zhaoyanbai@126.com>
2019-10-03 15:01:46 +0800

    initialize the clock tree of GD32VF103C_START to 108MHz

--- board/GD32VF103C_START/BSP/Src/mcu_init.c
+++ board/GD32VF103C_START/BSP/Src/mcu_init.c
@@ -1,6 +1,9 @@
 #include "mcu_init.h"
 
 void board_init() {
+
+    SystemInit();
+
     rcu_periph_clock_enable(RCU_GPIOA);
 
     gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_7);
