f4d5ff70b2e65a600c87e53ef36913c1691cb569
acevest <zhaoyanbai@126.com>
2019-10-16 18:16:42 +0800

    bumblebee use eclic_mtip_handler as systick handler

--- board/GigaDevice_GD32VF103C_START/BSP/Src/gd32vf103_it.c
+++ board/GigaDevice_GD32VF103C_START/BSP/Src/gd32vf103_it.c
@@ -1,5 +1,7 @@
 #include "tos.h"
-void SysTick_IRQHandler() {
+
+// systick handler
+void eclic_mtip_handler() {
     port_systick_config((uint32_t)k_cpu_cycle_per_tick);
     if (tos_knl_is_running()) {
         tos_knl_irq_enter();
