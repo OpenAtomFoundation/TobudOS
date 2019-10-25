ad7fb0af100ded8bdb306940dafae12b93130e9c
animala <102242739@qq.com>
2019-10-25 12:14:34 +0800

    123

---
+++ board/TI_MSP432P401R_LaunchPad/KEIL/hello_world/RTE/_TencentOS_tiny/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'TencentOS_tiny' 
+ * Target:  'TencentOS_tiny' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
