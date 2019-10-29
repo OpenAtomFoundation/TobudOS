ce71db8801fb6d7607d29faae4ed40528fb2a9cc
Chen Han <qq1433255094@outlook.com>
2019-10-29 15:44:59 +0800

    to focus mqtt_config.h

--- board/Linux_Posix/mqtt_demo/inc/mqtt_config.h
+++ board/Linux_Posix/mqtt_demo/inc/mqtt_config.h
@@ -1,6 +1,16 @@
 #ifndef  TOS_MQTT_CONFIG_H
 #define  TOS_MQTT_CONFIG_H
 
+#define MQTT_SERVER_IP          "111.230.189.156"
+#define MQTT_SERVER_PORT        "1883"
+#define MQTT_PRODUCT_ID         "$product"
+#define MQTT_DEV_NAME           "$dev"
+#define MQTT_CLIENT_ID          "$product$dev"
+#define MQTT_USR_NAME           "$product$dev;21010406;12365;4294967295"
+#define MQTT_PASSWORD           "$sign;hmacsha1"
+#define MQTT_SUBSCRIBE_TOPIC    "$product/$dev/$sub"
+#define MQTT_PUBLISH_TOPIC      "$product/$dev/$pub"
+
 #error please replace yourself server configuration
 /**
  * 1. run python tool
