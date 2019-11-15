1fd250764d3858733a13cfa14f6ea11e74359c8c
supowang <supowang@tencent.com>
2019-11-15 11:46:59 +0800

    add nb-iot demo for huawei ocean_connect
    add nb-iot demo for huawei ocean_connect

---
+++ devices/bc35_28_95_lwm2m/bc35_28_95_lwm2m.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------
+ * Tencent is pleased to support the open source community by making TencentOS
+ * available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ * If you have downloaded a copy of the TencentOS binary from Tencent, please
+ * note that the TencentOS binary is licensed under the BSD 3-Clause License.
+ *
+ * If you have downloaded a copy of the TencentOS source code from Tencent,
+ * please note that TencentOS source code is licensed under the BSD 3-Clause
+ * License, except for the third-party components listed below which are
+ * subject to different license terms. Your integration of TencentOS into your
+ * own projects may require compliance with the BSD 3-Clause License, as well
+ * as the other licenses applicable to the third-party components included
+ * within TencentOS.
+ *---------------------------------------------------------------------------*/
+
+#ifndef __BC35_28_95_LWM2M_H__
+#define __BC35_28_95_LWM2M_H__
+
+#include "tos_at.h"
+
+int bc35_28_95_sal_lwm2m_init(hal_uart_port_t uart_port);
+
+#endif /* __BC35_28_95_LWM2M_H__ */
+
