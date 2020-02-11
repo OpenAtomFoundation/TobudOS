932ae3495259ea9794fb8c063b6a5ec416bdc646
royye62 <royye62@gmail.com>
2020-02-11 22:36:08 +0800

    mini program: remove secret key

--- tools/Mini_Program/qcloud_iothub_mp_cloudfunctions_farmai/cloudfunctions/iothub-shadow-query/index.js
+++ tools/Mini_Program/qcloud_iothub_mp_cloudfunctions_farmai/cloudfunctions/iothub-shadow-query/index.js
@@ -7,19 +7,17 @@ const Credential = tencentcloud.common.Credential;
 const ClientProfile = tencentcloud.common.ClientProfile;
 const HttpProfile = tencentcloud.common.HttpProfile;
 
-let cred = new Credential("AKIDDDzjIeENdhosOg9T0LRXabO0B7pHhzQe", "f6IqCUirpeF1gLn3f0IWosjOqjOyTq2r");
-let httpProfile = new HttpProfile();
-httpProfile.endpoint = "iotcloud.tencentcloudapi.com";
-let clientProfile = new ClientProfile();
-clientProfile.httpProfile = httpProfile;
-let client = new IotcloudClient(cred, "ap-guangzhou", clientProfile);
-
-let req = new models.DescribeDeviceShadowRequest();
-
-let params = '{"ProductId":"U1BZWHF7F9","DeviceName":"dev_01"}'
-req.from_json_string(params);
-
 exports.main = async (event, context) => {
+  let cred = new Credential(event.SecretId, event.SecretKey);
+  let httpProfile = new HttpProfile();
+  httpProfile.endpoint = "iotcloud.tencentcloudapi.com";
+  let clientProfile = new ClientProfile();
+  clientProfile.httpProfile = httpProfile;
+  let client = new IotcloudClient(cred, "ap-guangzhou", clientProfile);
+  let req = new models.DescribeDeviceShadowRequest();
+  req.DeviceName = event.DeviceName;
+  req.ProductId = event.ProductId;
+
   return new Promise((resolve, reject)=>{
     client.DescribeDeviceShadow(req, function (errMsg, response) {
 
