3fa3cc358c4b4098dad9f898bb6e5bcacf278315
jiejietop <jiejietop@gmail.com>
2020-06-23 15:33:00 +0800

    fix mqttclient sample compilation error on board-EVB_LN882x ...

--- board/EVB_LN882x/BSP/Src/mqtt_task.c
+++ board/EVB_LN882x/BSP/Src/mqtt_task.c
@@ -9,15 +9,11 @@
 static void tos_topic_handler(void* client, message_data_t* msg)
 {
     (void) client;
-    LOG_I("-----------------------------------------------------------------------------------");
-    LOG_I("%s:%d %s()...\ntopic: %s, qos: %d. \nmessage:\n\t%s\n", __FILE__, __LINE__, __FUNCTION__, 
+    MQTT_LOG_I("-----------------------------------------------------------------------------------");
+    MQTT_LOG_I("%s:%d %s()...\ntopic: %s, qos: %d. \nmessage:\n\t%s\n", __FILE__, __LINE__, __FUNCTION__, 
             msg->topic_name, msg->message->qos, (char*)msg->message->payload);
-    LOG_I("-----------------------------------------------------------------------------------\n");
+    MQTT_LOG_I("-----------------------------------------------------------------------------------\n");
 }
-
-
-mqtt_client_t client;
-client_init_params_t init_params;
 
 extern void TCPIP_Init(void);
 
@@ -25,29 +21,31 @@ void mqtt_iothub(void)
 {
     int error;
     char buf[80] = { 0 };
+    
+    mqtt_client_t *client = NULL;
+    
     mqtt_message_t msg;
     memset(&msg, 0, sizeof(msg));
     
     sprintf(buf, "welcome to mqttclient, this is a publish test...");
     
-    init_params.read_buf_size = 1024;
-    init_params.write_buf_size = 1024;
-    init_params.connect_params.network_params.addr = "iotcloud-mqtt.gz.tencentdevices.com";
-    init_params.connect_params.network_params.port = "1883";
-    init_params.connect_params.user_name = "RUAP1R610Vsupowang;12010126;FTDC9;1623108655"; 
-    init_params.connect_params.password = "29e68d809ac4ac0e41bf30cc77cd377d30e7af03f3c76b0c50fa27a2c59e174b;hmacsha256"; 
-    init_params.connect_params.client_id = "RUAP1R610Vsupowang";
-    init_params.connect_params.clean_session = 1;
+    mqtt_log_init();
 
-    log_init();
-
-    mqtt_init(&client, &init_params);
-
-    error = mqtt_connect(&client);
+    client = mqtt_lease();
     
-    LOG_D("mqtt connect error is %#x", error);
+    mqtt_set_port(client, "1883");
+    mqtt_set_host(client, "iotcloud-mqtt.gz.tencentdevices.com");
+    mqtt_set_client_id(client, "RUAP1R610Vsupowang");
+    mqtt_set_user_name(client, "RUAP1R610Vsupowang;12010126;FTDC9;1623108655");
+    mqtt_set_password(client, "29e68d809ac4ac0e41bf30cc77cd377d30e7af03f3c76b0c50fa27a2c59e174b;hmacsha256");
+    mqtt_set_clean_session(client, 1);
     
-    mqtt_subscribe(&client, "RUAP1R610V/supowang/data", QOS0, tos_topic_handler);
+
+    error = mqtt_connect(client);
+    
+    MQTT_LOG_D("mqtt connect error is %#x", error);
+    
+    mqtt_subscribe(client, "RUAP1R610V/supowang/data", QOS0, tos_topic_handler);
 
     memset(&msg, 0, sizeof(msg));
 
@@ -56,7 +54,7 @@ void mqtt_iothub(void)
         msg.qos = QOS0;
         msg.payload = (void *) buf;
         
-        mqtt_publish(&client, "RUAP1R610V/supowang/data", &msg);
+        mqtt_publish(client, "RUAP1R610V/supowang/data", &msg);
         
         tos_task_delay(4000); 
     }
