e82a96102bf8c9159bcf08909f627d78b8b8118c
David Lin <linpeng1577@gmail.com>
2020-07-31 20:05:57 +0800

    Update tos_farm_ai_config.h

--- examples/farm_ai_demo/tos_farm_ai_config.h
+++ examples/farm_ai_demo/tos_farm_ai_config.h
@@ -1,5 +1,5 @@
-#ifndef __TOS_FARM_AI_CONFIG_H__  
-#define __TOS_FARM_AI_CONFIG_H__
+#ifndef _TOS_FARM_AI_CONFIG_H_  
+#define _TOS_FARM_AI_CONFIG_H_
 
 #define MQTT_SERVER_IP          "iotcloud-mqtt.gz.tencentdevices.com"
 #define MQTT_SERVER_PORT        "1883"
@@ -11,4 +11,4 @@
 #define MQTT_SUBSCRIBE_TOPIC    "LOLCIH8A14/AiDev1/control"
 #define MQTT_PUBLISH_TOPIC      "LOLCIH8A14/AiDev1/data"
 
-#endif /* __TOS_FARM_AI_CONFIG_H__ */
+#endif /* _TOS_FARM_AI_CONFIG_H_ */
