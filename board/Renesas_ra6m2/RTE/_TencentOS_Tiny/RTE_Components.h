14f9024c09d3140ad1fa5acfdbe9f87bb610343e
supowang <supowang@tencent.com>
2021-04-02 15:14:09 +0800

    add Renesas RA6M2 support

---
+++ board/Renesas_ra6m2/RTE/_TencentOS_Tiny/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ra6m2_tencent_sample' 
+ * Target:  'TencentOS_Tiny' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
