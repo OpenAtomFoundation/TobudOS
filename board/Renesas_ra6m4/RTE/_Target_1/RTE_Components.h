8bc0b76112dcc4d699ad72aae00f63eabdad52ea
supowang <supowang@tencent.com>
2021-04-13 11:07:44 +0800

    add new board RA6M4

---
+++ board/Renesas_ra6m4/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '20210401_ra6m4_tencent' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
