9311ef85d3d15aa9c057d4012f7524ea1b0b10bc
mculover666 <2412828003@qq.com>
2021-04-16 16:55:49 +0800

    add support for connect_with_size,update verison to 2.3.1

--- kernel/core/include/tos_version.h
+++ kernel/core/include/tos_version.h
@@ -24,9 +24,9 @@
 // patch is updated for patch changes/bug fixes that should not need user code changes
 
 #define TOS_VERSION_MAJOR   0x02
-#define TOS_VERSION_MINOR   0x02
+#define TOS_VERSION_MINOR   0x03
 #define TOS_VERSION_PATCH   0x01
-#define TOS_VERSION "2.2.1"
+#define TOS_VERSION "2.3.1"
 
 #endif /* _TOS_VERSION_H_ */
 
