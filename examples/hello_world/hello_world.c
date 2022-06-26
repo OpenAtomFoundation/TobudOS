c26346521566a53189b68486d618ccba578c46fb
mculover666-3 <name>
2022-06-26 23:33:32 +0800

    this is a task2

--- examples/hello_world/hello_world.c
+++ examples/hello_world/hello_world.c
@@ -20,7 +20,7 @@ void task1_entry(void *arg)
 void task2_entry(void *arg)
 {
     while (1) {
-        printf("***I am task2\r\n");
+        printf("** this is a task2\r\n");
         tos_task_delay(1000);
     }
 }
