484209741d0a0d0c8abe0b510e4c43036c380808
ChenWanchun <2696992931@qq.com>
2023-08-09 13:16:25 +0800

    this is a demo

--- examples/hello_world/hello_world.c
+++ examples/hello_world/hello_world.c
@@ -1,17 +1,17 @@
 #include "tos_k.h"
 
-#define TASK1_STK_SIZE       1024
+#define TASK1_STK_SIZE 1024
 k_task_t task1;
 uint8_t task1_stk[TASK1_STK_SIZE];
 
-
-#define TASK2_STK_SIZE       1024
+#define TASK2_STK_SIZE 1024
 k_task_t task2;
 uint8_t task2_stk[TASK2_STK_SIZE];
 
 void task1_entry(void *arg)
 {
-    while (1) {
+    while (1)
+    {
         printf("###I am task1\r\n");
         tos_task_delay(2000);
     }
@@ -19,16 +19,16 @@ void task1_entry(void *arg)
 
 void task2_entry(void *arg)
 {
-    while (1) {
+    while (1)
+    {
         printf("***I am task2\r\n");
         tos_task_delay(1000);
     }
 }
 
-
 void application_entry(void *arg)
 {
+    print("This is a demo");
     tos_task_create(&task1, "task1", task1_entry, NULL, 3, task1_stk, TASK1_STK_SIZE, 0); // Create task1
-    tos_task_create(&task2, "task2", task2_entry, NULL, 3, task2_stk, TASK2_STK_SIZE, 0);// Create task2
+    tos_task_create(&task2, "task2", task2_entry, NULL, 3, task2_stk, TASK2_STK_SIZE, 0); // Create task2
 }
-
